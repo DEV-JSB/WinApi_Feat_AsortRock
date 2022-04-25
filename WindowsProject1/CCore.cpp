f3ffc66f1627fcff61d4b581caa4c2bf7f0b033c
DEV_JSB <mvat1225@gmail.com>
2022-04-25 16:36:36 +0900

    더블 버퍼링 구현

--- WindowsProject1/CCore.cpp
+++ WindowsProject1/CCore.cpp
@@ -11,6 +11,8 @@ CCore::CCore()
 	: m_hWnd(0)
 	, m_ptResolution{}
 	, m_hDC(0)
+	, m_hBit(0)
+	, m_memDC(0)
 {
 
 }
@@ -20,6 +22,9 @@ CObject g_obj;
 CCore::~CCore()
 {
 	ReleaseDC(m_hWnd, m_hDC);
+	
+	DeleteDC(m_memDC);
+	DeleteObject(m_hBit);
 }
 
 
@@ -38,6 +43,15 @@ int CCore::Init(HWND _hWnd, POINT _ptResolution)
 
 	// 내부적으로 따로 그리기 전용 DC 생성
 	m_hDC = GetDC(m_hWnd);
+
+
+	// 이중 버퍼링 용도의 비트맵과 DC 를 만든다
+	m_hBit = CreateCompatibleBitmap(m_hDC, m_ptResolution.x, m_ptResolution.y);
+	m_memDC = CreateCompatibleDC(m_hDC);
+
+	HBITMAP hOldBit = (HBITMAP)SelectObject(m_memDC, m_hBit);
+	DeleteObject(hOldBit);
+
 
 	//Manager 초기화
 	CTimeMgr::GetInst()->Init();
@@ -81,11 +95,23 @@ void CCore::update()
 
 void CCore::render()
 {
+	// 화면 Clear 화면 재구성
+
+	Rectangle(m_memDC, -1, -1, m_ptResolution.x + 1, m_ptResolution.y + 1);
+
+
+	// 그리기
 	Vec2 vPos = g_obj.GetPos();
 	Vec2 vScale = g_obj.GetScale();
-	Rectangle(m_hDC, 10, 10, 110, 110);
-	Rectangle(m_hDC , (int)vPos.x - vScale.x / 2.f
+	Rectangle(m_memDC, (int)vPos.x - vScale.x / 2.f
 					, (int)vPos.y - vScale.y / 2.f
 					, (int)vPos.x + vScale.x / 2.f
 					, (int)vPos.y + vScale.y / 2.f);
+	Rectangle(m_memDC, (int)vPos.x - vScale.x / 2.f -100
+		, (int)vPos.y - vScale.y / 2.f -100
+		, (int)vPos.x + vScale.x -100
+		, (int)vPos.y + vScale.y-100);
+
+	BitBlt(m_hDC,0,0,m_ptResolution.x,m_ptResolution.y,
+			m_memDC, 0, 0,SRCCOPY);
 }
--- WindowsProject1/CCore.h
+++ WindowsProject1/CCore.h
@@ -10,6 +10,9 @@ private:
 	HWND	m_hWnd;			//메인 윈도우 핸들
 	POINT	m_ptResolution;	//해상도
 	HDC		m_hDC; // 메인 윈도우에 Draw 할 DC 
+
+	HBITMAP	m_hBit;
+	HDC		m_memDC;
 public:
 	int Init(HWND _hWnd, POINT _ptResolution);
 	void progress();
