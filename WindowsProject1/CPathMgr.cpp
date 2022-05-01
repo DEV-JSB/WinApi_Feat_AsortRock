909b7b685c7df0ccca460e21c1b07ea4685333d6
DEV_JSB <mvat1225@gmail.com>
2022-05-01 20:07:31 +0900

    상위폴더 찾아서 빠져 나오고 다시 경로 재지정

--- WindowsProject1/CPathMgr.cpp
+++ WindowsProject1/CPathMgr.cpp
@@ -5,24 +5,31 @@ CPathMgr::CPathMgr()
 	:m_szContentPath{}
 {
 }
-
 void CPathMgr::Init()
 {
-	GetCurrentDirectory(255,m_szContentPath);
+	GetCurrentDirectory(255, m_szContentPath);
 
 	int iLen = wcslen(m_szContentPath);
 
 
+	// 상위 폴더로 나간다
+	for (int i = iLen - 1; 0 <= i; --i)
+	{
+		if ('\\' == m_szContentPath[i])
+		{
+			m_szContentPath[i] = '\0';
+			break;
+		}
+	}
 
-	wcscat_s(m_szContentPath, 255, L"\\content\\");
+	wcscat_s(m_szContentPath, 255, L"\\bin\\content\\");
 	// ->bin\\content\\
 
 
-	//SetWindowText(CCore::GetInst()->GetMainHwnd(), m_szContentPath);
+	SetWindowText(CCore::GetInst()->GetMainHwnd(), m_szContentPath);
 
 
 }
-
 
 CPathMgr::~CPathMgr()
 {
